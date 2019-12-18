84128fd83ea31102fa0c0688734c1af3fa5c8eff
Diana Popa <diana.popa@fortech.ro>
2019-12-18 18:42:24 +0200

    Theme Provider

--- app/index.js
+++ app/index.js
@@ -3,13 +3,28 @@ import ReactDOM from 'react-dom';
 // import Popular from './components/Popular';
 import Battle from './components/Battle';
 import './index.css';
+import { ThemeProvider } from "./contexts/Theme";
 
 class App extends React.Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            theme: 'light',
+            toggleTheme: () => {
+                this.setState(({ theme }) => ({
+                    theme: theme === 'light' ? 'dark' : 'light'
+                }))
+            }
+        }
+    }
     render() {
         return (
-            <div className='container'>
-                <Battle />
-            </div>
+            <ThemeProvider value={theme}>
+                <div className='container'>
+                    <Battle />
+                </div>
+            </ThemeProvider>
         )
     }
 }
