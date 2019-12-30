55097375b133b5286c2ffd50a702f94d353a563f
diana1002 <diana.popa1002@gmail.com>
2019-12-30 14:32:09 +0200

    production build

--- webpack.config.js
+++ webpack.config.js
@@ -11,10 +11,10 @@ module.exports = {
     module: {
         rules: [
             { test: /\.(js)$/, use: 'babel-loader' },
-            { test: /\.css$/, use: ['style-loader', 'css-loader'] } 
+            { test: /\.css$/, use: ['style-loader', 'css-loader'] }
         ]
     },
-    mode: 'development',
+    mode: (process.env.NODE_ENV === 'production') ? 'production' : 'development',
     plugins: [
         new HtmlWebpackPlugin({
             template: 'app/index.html'
